7ac4f7a00f17fb98e8cf22634257d9929e81d354
Frank <sptunjai@gmail.com>
2017-10-26 13:46:51 +0000

    end 4.10 in book

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,50 @@ class MoviesController < ApplicationController
     end 
     
     def show
-        id = params[:id] # retrieve movie ID from URI route
-        @movie = Movie.find(id) # look up movie by unique ID
-        # will render app/views/movies/show.html.haml by default
+         @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
+        # BUG: we should check @movie for validity here!
     end
     
+    def new
+        # default: render 'new' template
+    end
+    
+    def create
+        @movie = Movie.create!(movie_params)
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
+    
+    def edit
+        @movie = Movie.find params[:id]
+    end
+ 
+    
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(movie_params)
+        respond_to do |client_wants|
+            client_wants.html {  redirect_to movie_path(@movie)  } # as before
+            client_wants.xml  {  render :xml => @movie.to_xml    }
+        end
+    end
+    
+    #def update
+    #    @movie = Movie.find params[:id]
+    #    @movie.update_attributes!(movie_params)
+    #    flash[:notice] = "#{@movie.title} was successfully updated."
+    #    redirect_to movie_path(@movie)
+    #end
+    
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
+    
+    private
+    def movie_params
+        params.require(:movie).permit!
+    end
 end
